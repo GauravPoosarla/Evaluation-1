17b7c0645b5c48f7168dbe46e10d3e9257a65641
GauravPoosarla <poosarlagaurav@gmail.com>
2023-01-18 12:54:22 +0530

    feat: add tests for calculating the best score

--- bowlingGameScore/bowlingScore.test.js
+++ bowlingGameScore/bowlingScore.test.js
@@ -13,14 +13,8 @@ describe('calculates score of one game', () => {
   });
 });
 
-describe('changing the capitalisation of every letter using arrow function', () => {
-  test('"MeRRy hAD a LITTle lAMp" should result in "mErrY Had A littLE LamP"', () => {
-    expect(changeCapitalisationUsingArrowFunction('MeRRy hAD a LITTle lAMp')).toBe('mErrY Had A littLE LamP');
-  });
-  test('"I am Gaurav Poosarla" should result in "i AM gAURAV pOOSARLA"', () => {
-    expect(changeCapitalisationUsingArrowFunction('I am Gaurav Poosarla')).toBe('i AM gAURAV pOOSARLA');
-  });
-  test('"IWorkAtMcKinsey" should result in "iwORKaTmCkINSEY"', () => {
-    expect(changeCapitalisationUsingArrowFunction('IWorkAtMcKinsey')).toBe('iwORKaTmCkINSEY');
+describe('returns the best score for the set of given rolls', () => {
+  it('should return 90 when input is [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]', () => {
+    expect(getScoreOneGame([[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]])).toBe(90);
   });
 });
