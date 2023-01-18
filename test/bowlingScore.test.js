5334a67de6a98e0e2a36f5764d85b0ca6f1ff8b5
GauravPoosarla <poosarlagaurav@gmail.com>
2023-01-18 18:20:05 +0530

    feat: add tests

---
+++ test/bowlingScore.test.js
@@ -0,0 +1,20 @@
+const { getScore } = require('../src/bowlingScore');
+const { getBestScore } = require('../src/bowlingScore');
+
+describe('Calculate bowling score of one roll', () => {
+  it('should return 90 when input is [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]', () => {
+    expect(getScoreOneGame([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+  });
+  it('should return 30 when input is [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]', () => {
+    expect(getScoreOneGame([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
+  });
+  it('should return 16 when input is [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]', () => {
+    expect(getScoreOneGame([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
+  });
+});
+
+describe('returns the best score for the set of given rolls', () => {
+  it('should return 90 when input is [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]]', () => {
+    expect(bestScoreCalculator([[3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]])).toBe(90);
+  });
+});
